546aaed93d4e5b4e9724f4faa6d2ed5ba0d1bc4e
xavier tiang <E0538446@u.nus.edu>
2022-03-17 11:28:35 +0800

    changes

--- sql/AppStoreClean.sql
+++ sql/AppStoreClean.sql
@@ -3,8 +3,7 @@
   Cleaning script
 
 *******************/
-
-DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Rental;
 DROP TABLE IF EXISTS GPU_Listing;
 DROP TABLE IF EXISTS GPU;
-DROP TABLE IF EXISTS Rental;
+DROP TABLE IF EXISTS User1;
