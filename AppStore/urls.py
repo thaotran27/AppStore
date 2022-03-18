7a9ed4ed42e5f7eb9e2ca4a7351a9c4a7703e32b
ChangTng1 <101157125+ChangTng1@users.noreply.github.com>
2022-03-18 23:30:52 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('add', app.views.add, name='add'),
     path('view/<str:id>', app.views.view, name='view'),
     path('edit/<str:id>', app.views.edit, name='edit'),
-    path('', app.views.index, name='index')
+    path('', app.views.index, name='index'),
     path('listing', app.views.listing, name = 'listing')
 ]
