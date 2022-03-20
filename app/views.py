067a629f17251afdef272428c8a612268e6def52
xavier tiang <E0538446@u.nus.edu>
2022-03-20 17:36:57 +0800

    updates

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.db import connection
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
 
 # Create your views here.
 def index(request):
@@ -141,3 +144,41 @@ def view_listing(request, id):
     result_dict = {'view_listing': view_listing}
 
     return render(request,'app/view_listing.html',result_dict)
+
+
+def rental(request, Listingid):
+    """Shows the main page"""
+    context = {}
+    status = ''
+
+    if request.POST:
+        ## Check if customerid is already in the table
+        with connection.cursor() as cursor:
+
+            cursor.execute("SELECT * FROM GPU_Listing WHERE Listingid = %s", [Listingid])
+            listing = cursor.fetchone()
+            ## No customer with same id
+            if (datetime.strptime(request.POST['Start_day'], '%Y-%m-%d').date() >= listing[4] and datetime.strptime(request.POST['End_day'], '%Y-%m-%d').date() <= listing[5]):
+                ##TODO: date validation
+                cursor.execute("INSERT INTO Rental VALUES (%s, %s, %s, %s, %s, %s)"
+                        , [request.POST['Borrower_id'], listing[1], listing[2],
+                           int(Listingid) , request.POST['Start_day'], request.POST['End_day']])
+                cursor.execute("DELETE FROM GPU_Listing WHERE Listingid = %s", [Listingid])
+                cursor.execute("SELECT * FROM GPU_Listing g1 WHERE g1.listingid >= all (SELECT g2.listingid FROM GPU_Listing g2)")
+                last_entry = cursor.fetchone()
+                if (datetime.strptime(request.POST['Start_day'], '%Y-%m-%d').date() == listing[4]):
+                    cursor.execute("INSERT INTO GPU_Listing VALUES(%s, %s,%s,%s,%s,%s,%s)", [last_entry[0] + 1 ,listing[1], listing[2], listing[3], 
+                                                                                         datetime.strptime(request.POST['End_day'], '%Y-%m-%d').date() + timedelta(days = 1), listing[5], listing[6]])
+                if (datetime.strptime(request.POST['Start_day'], '%Y-%m-%d').date() > listing[4]):
+                    cursor.execute("INSERT INTO GPU_Listing VALUES(%s, %s,%s,%s,%s,%s,%s)", [last_entry[0] + 1 ,listing[1], listing[2], listing[3], 
+                                                                                         listing[4], datetime.strptime(request.POST['Start_day'], '%Y-%m-%d').date()  - timedelta(days = 1), listing[6]])
+                    cursor.execute("INSERT INTO GPU_Listing VALUES(%s, %s,%s,%s,%s,%s,%s)", [last_entry[0] + 2 ,listing[1], listing[2], listing[3], 
+                                                                                         datetime.strptime(request.POST['End_day'], '%Y-%m-%d').date() + timedelta(days = 1), listing[5], listing[6]])
+                return redirect('index')    
+            else:
+                status = 'Invalid Rental Dates'
+
+
+    context['status'] = status
+ 
+    return render(request, "app/rental.html", context)
