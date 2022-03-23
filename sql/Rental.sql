341574dfcab0ed57171515e07d94294bd71caac8
quangdat191 <tranquangdat191@gmail.com>
2022-03-23 18:12:05 +0800

    update database

--- sql/Rental.sql
+++ sql/Rental.sql
@@ -4,8 +4,8 @@
 
 ****************************/
 
-INSERT INTO Rental VALUES ('gpleven4', 'Quadro RTX 5000', 'NVIDIA', '4', '2021-04-17', '2021-04-18');
-
-
-
-
+--INSERT INTO Rental VALUES ('cgoodrickez', 'T1000 8 GB', 'NVIDIA', '15', '2021-01-08', '2021-01-09');
+--INSERT INTO Rental VALUES ('dswannie10', 'GeForce MX230', 'NVIDIA', '18', '2021-03-05', '2021-03-06');
+--INSERT INTO Rental VALUES ('pbertomier11', 'Tesla V100 DGXS 32 GB', 'NVIDIA', '6', '2021-02-24', '2021-02-25');
+--INSERT INTO Rental VALUES ('cimison12', 'P102-100', 'NVIDIA', '9', '2021-01-26', '2021-01-07');
+--INSERT INTO Rental VALUES ('mpenbarthy13', 'Quadro P520 Mobile', 'NVIDIA', '28', '2021-02-18', '2021-02-19');
