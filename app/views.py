711af5dcbecd625adb75c850ba49b265e0d5b19d
xavier tiang <E0538446@u.nus.edu>
2022-03-25 00:30:58 +0800

    changes

--- app/views.py
+++ app/views.py
@@ -226,7 +226,7 @@ def rental(request, Listingid):
         cursor.execute("SELECT * FROM GPU_Listing WHERE Listingid = %s", [Listingid])
         GPU_choice = cursor.fetchall()
         cursor.execute("SELECT * FROM User1 WHERE Email = %s", [request.session['email']])
-        Borrower_details = cursor.fetchone()
+        Borrower_details = cursor.fetchall()
     if request.POST:
         ## Check if customerid is already in the table
         with connection.cursor() as cursor:
