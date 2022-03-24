f234b063ec5128db59c3dea61ad26d8553f66192
xavier tiang <E0538446@u.nus.edu>
2022-03-25 01:28:53 +0800

    changes

--- app/views.py
+++ app/views.py
@@ -235,11 +235,11 @@ def rental(request, Listingid):
             ## No customer with same id
             if (datetime.strptime(request.POST['Start_day'], '%Y-%m-%d').date() >= listing[4] and datetime.strptime(request.POST['End_day'], '%Y-%m-%d').date() <= listing[5]):
                 ##TODO: date validation
-                #cursor.execute("INSERT INTO Rental VALUES (%s, %s, %s, %s, %s, %s)",[request.POST['Borrower_id'], listing[1], listing[2],
-                #           int(Listingid) , request.POST['Start_day'], request.POST['End_day']])
-                cursor.execute("INSERT INTO Rental VALUES (%s, %s, %s, %s, %s, %s)"
-                        , [Borrower_details[3], listing[1], listing[2],
+                cursor.execute("INSERT INTO Rental VALUES (%s, %s, %s, %s, %s, %s)",[request.POST['Borrower_id'], listing[1], listing[2],
                            int(Listingid) , request.POST['Start_day'], request.POST['End_day']])
+                #cursor.execute("INSERT INTO Rental VALUES (%s, %s, %s, %s, %s, %s)"
+                #        , [Borrower_details[3], listing[1], listing[2],
+                #           int(Listingid) , request.POST['Start_day'], request.POST['End_day']])
                 #cursor.execute("DELETE FROM GPU_Listing WHERE Listingid = %s", [Listingid])
                 cursor.execute("SELECT * FROM GPU_Listing g1 WHERE g1.listingid >= all (SELECT g2.listingid FROM GPU_Listing g2)")
                 last_entry = cursor.fetchone()
