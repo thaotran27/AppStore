b0a68d7adefea0725a6b6bb61c96319203134b86
xavier tiang <E0538446@u.nus.edu>
2022-04-01 22:56:22 +0800

    changes

--- sql/GPU_rentalSchema.sql
+++ sql/GPU_rentalSchema.sql
@@ -10,10 +10,12 @@ CREATE TABLE IF NOT EXISTS User1(
  Email VARCHAR(64) UNIQUE NOT NULL,
  Customerid VARCHAR(16) PRIMARY KEY,
  Wallet_balance NUMERIC NOT NULL CHECK (wallet_balance >= 0),
- Phone_number NUMERIC NOT NULL,
+ Phone_number NUMERIC NOT NULL CHECK (Phone_number <= 9999999999 and Phone_number > 999999999),
  Pass_word VARCHAR(64) NOT NULL,
- Credit_card_number NUMERIC NOT NULL,
- Credit_card_type VARCHAR(64) NOT NULL);
+ Credit_card_number NUMERIC NOT NULL, --CHECK (IF (Credit_card_type = 'mastercard', CHECK (Credit_card_number <= 5999999999999999 and Credit_card_number > 4999999999999999), Credit_card_type = Credit_card_type) or
+											--IF (Credit_card_type = 'visa', CHECK ((Credit_card_number <= 3999999999999999 and Credit_card_number > 2999999999999999) or (Credit_card_number <= 3999999999999 and Credit_card_number > 2999999999999)), Credit_card_type = Credit_card_type) or
+											--IF (Credit_card_type = 'americanexpress', CHECK ((Credit_card_number <= 349999999999999 and Credit_card_number > 339999999999999) or (Credit_card_number <= 379999999999999 and Credit_card_number > 369999999999999)), Credit_card_type = Credit_card_type))
+ Credit_card_type VARCHAR(64) NOT NULL CHECK (Credit_card_type = 'mastercard' or Credit_card_type = 'visa' or Credit_card_type = 'americanexpress'));
 	
  CREATE TABLE IF NOT EXISTS GPU(
  GPU_model VARCHAR(32),
@@ -35,8 +37,8 @@ CREATE TABLE IF NOT EXISTS User1(
  GPU_brand VARCHAR(32),
  FOREIGN KEY (GPU_model, GPU_brand) REFERENCES GPU(GPU_model, GPU_brand) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
  Customerid VARCHAR(16) REFERENCES User1(Customerid) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
- Available_start_day DATE NOT NULL,
- Available_end_day DATE NOT NULL,
+ Available_start_day DATE NOT NULL CHECK (Available_start_day <= Available_end_day),
+ Available_end_day DATE NOT NULL CHECK(Available_end_day >= Available_start_day),
  Price NUMERIC NOT NULL CHECK (Price >= 0));
 
 CREATE TABLE IF NOT EXISTS GPU_Listing_Archive(
@@ -53,6 +55,6 @@ CREATE TABLE IF NOT EXISTS GPU_Listing_Archive(
  GPU_brand VARCHAR(32),
  FOREIGN KEY (GPU_model, GPU_brand) REFERENCES GPU(GPU_model, GPU_brand) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
  Listingid NUMERIC CHECK (Listingid >= 0) REFERENCES GPU_Listing_Archive(Listingid) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
- Start_day DATE NOT NULL,
- End_day DATE NOT NULL);
+ Start_day DATE NOT NULL CHECK (Start_day <= End_day),
+ End_day DATE NOT NULL CHECK (End_day >= Start_day));
 
